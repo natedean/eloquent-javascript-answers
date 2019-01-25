36629ffbf1e7c3910c59ab7017267e2938c8e4a3
Nate Dean <nate.dean@selectquote.com>
2019-01-25 14:28:26 -0600

    Adds 6.vec.js file

---
+++ 6.vec.js
@@ -0,0 +1,22 @@
+module.exports = class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plus(vector) {
+        const x = this.x + vector.x;
+        const y = this.y + vector.y;
+        return new Vec(x, y);
+    }
+
+    minus(vector) {
+        const x = this.x - vector.x;
+        const y = this.y - vector.y;
+        return new Vec(x, y);
+    }
+
+    get length() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+};
